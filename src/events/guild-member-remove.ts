1bebe32fcd7f7a382dbb9f9418ebcc3f0f9ee40d
pow <abinovalfauzi@proton.me>
2025-09-18 07:49:24 +0700

    🛡️ feat: add guild member remove event for cleanup

---
+++ src/events/guild-member-remove.ts
@@ -0,0 +1,10 @@
+import { Events, GuildMember } from "discord.js";
+import { BotClient } from "../client";
+import { handleBoosterStopped } from "~/services/booster-services";
+
+export const name = Events.GuildMemberUpdate;
+export const once = false;
+
+export async function execute(member: GuildMember, _: BotClient) {
+    await handleBoosterStopped(member);
+}
